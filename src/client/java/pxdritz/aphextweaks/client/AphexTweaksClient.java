622f4db4dd21113cc05c52753526ac48ff2c4cea
EdonMe Studios <edonmestudios@outlook.com>
2026-07-01 19:01:58 -0400

    feat: basic structure of the mod, are made

---
+++ src/client/java/pxdritz/aphextweaks/client/AphexTweaksClient.java
@@ -0,0 +1,10 @@
+package pxdritz.aphextweaks.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AphexTweaksClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/pxdritz/aphextweaks/client/AphexTweaksDataGenerator.java
@@ -0,0 +1,11 @@
+package pxdritz.aphextweaks.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AphexTweaksDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
